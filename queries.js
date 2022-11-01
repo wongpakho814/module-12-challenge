0e6fb963ec940149b71a8130337222732ee8fb89
wongpakho814 <wongpakho814@gmail.com>
2022-11-01 20:07:45 +1100

    Implemented functionality for adding a new employee

--- queries.js
+++ queries.js
@@ -2,18 +2,78 @@ const mysql = require("mysql2");
 const cTable = require("console.table");
 const inquirer = require("inquirer");
 
-// List of SQL queries used later
-const viewAllEmployeeSQL = `SELECT e.id, first_name, last_name, r.title, d.name AS department, r.salary, 
-    (SELECT CONCAT_WS(" ", first_name, last_name) FROM employee AS e2 WHERE e2.id = e.manager_id) AS manager
-FROM (role AS r INNER JOIN department AS d ON r.department_id = d.id)
-INNER JOIN employee AS e ON e.role_id = r.id;`;
-const addEmployeeSQL = ``;
+// Function that returns all role titles from the role table as an array
+function getRoleTitles(db) {
+  return new Promise((resolve, reject) => {
+    db.query(`SELECT title FROM role`, (err, result) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(result.map((role) => role.title));
+      }
+    });
+  });
+}
+
+// Function to get the role_id specified by the role title
+function getRoleId(db, data) {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `SELECT id FROM role WHERE title = "${data.role}"`,
+        (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result.map((role_id) => role_id.id));
+          }
+        }
+      );
+    });
+}
+
+// Function to get the manager_id specified by the name of the employee
+function getManagerId(db, data) {
+    return new Promise((resolve, reject) => {
+      db.query(
+        `SELECT id FROM employee WHERE CONCAT_WS(" ", first_name, last_name) = "${data.manager}"`,
+        (err, result) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(result.map((manager_id) => manager_id.id));
+          }
+        }
+      );
+    });
+}
+
+// Function that returns all employees names (first+last name) from the employee table as an array
+function getEmployeeNames(db) {
+  return new Promise((resolve, reject) => {
+    db.query(
+      `SELECT CONCAT_WS(" ", first_name, last_name) AS name FROM employee`,
+      (err, result) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(result.map((employee) => employee.name));
+        }
+      }
+    );
+  });
+}
 
 // Function to perform the query that prints out all employees
 function viewAllEmployee(db) {
+  const sql = `SELECT e.id, first_name, last_name, r.title, d.name AS department, r.salary, 
+    (SELECT CONCAT_WS(" ", first_name, last_name) FROM employee AS e2 WHERE e2.id = e.manager_id) AS manager
+FROM (role AS r INNER JOIN department AS d ON r.department_id = d.id)
+INNER JOIN employee AS e ON e.role_id = r.id 
+ORDER BY e.id;`;
+
   return new Promise((resolve, reject) => {
     db.promise()
-      .query(viewAllEmployeeSQL)
+      .query(sql)
       .then(([rows, fields]) => {
         console.log("\n");
         console.table(rows);
@@ -23,22 +83,12 @@ function viewAllEmployee(db) {
   });
 }
 
-// Function that returns all role titles from the role table as an array
-function getRoleTitles(db) {
-    return new Promise((resolve, reject) => {
-      db.query(`SELECT title FROM role`, (err, result) => {
-        if (err) {
-          reject(err);
-        } else {
-          resolve(result.map((role) => role.title));
-        }
-      });
-    });
-}
-
 // Function to prompt the user about the information of the employee to be added, and perform the SQL query based on that
 async function addEmployee(db) {
-  const roles = await getRoleTitles(db);
+  const roles = await getRoleTitles(db); // Getting an array of titles for the choice parameter
+  const names = await getEmployeeNames(db); // Getting an array of employee names for the choice parameter
+  names.unshift("None");
+
   return new Promise((resolve, reject) => {
     inquirer
       .prompt([
@@ -59,12 +109,30 @@ async function addEmployee(db) {
           choices: roles,
         },
         {
-          type: "input",
+          type: "list",
           name: "manager",
           message: "Who is the employee's manager?",
+          choices: names,
         },
       ])
-      .then((data) => {
+      .then(async (data) => {
+        const role_id = await getRoleId(db, data); // Converting the role title to role_id
+        const manager_id = await getManagerId(db, data); // Converting the employee name to manager_id
+        const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+        const params = [
+          data.firstName,
+          data.lastName,
+          role_id[0],
+          manager_id[0]
+        ];
+
+        db.promise()
+          .query(sql, params)
+          .then(([rows, fields]) => {
+            resolve(rows);
+          })
+          .catch(console.log);
+
         console.log(`Added ${data.firstName} ${data.lastName} to the database`);
         resolve(data);
       });
